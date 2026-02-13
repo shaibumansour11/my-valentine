7156d4f51eebcae6c3f2177d8e4bb7c98c08689e
Shaibu Mansour <shaibumansour10@gmail.com>
2026-02-13 22:47:38 +0300

    feat: Initialize Prisma client with `PrismaPg` adapter for database connection.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,7 @@
 // lib/prisma.ts
 
-import { PrismaClient } from "../app/generated/prisma/client";
+// import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as {
